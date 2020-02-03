5bebc597e68a79ed62023d68a09006fd591bf3b3
Ryan Bachrach <ryanbachrach9@gmail.com>
2020-02-03 10:53:50 -0600

    Initial Commit

---
+++ Studio Ghibli Films/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Studio Ghibli Films
+//
+//  Created by ryan bachrach on 2/3/20.
+//  Copyright © 2020 Novak Software Development. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Studio Ghibli FilmsTests/Studio_Ghibli_FilmsTests.swift
@@ -0,0 +1,34 @@
+//
+//  Studio_Ghibli_FilmsTests.swift
+//  Studio Ghibli FilmsTests
+//
+//  Created by ryan bachrach on 2/3/20.
+//  Copyright © 2020 Novak Software Development. All rights reserved.
+//
+
+import XCTest
+@testable import Studio_Ghibli_Films
+
+class Studio_Ghibli_FilmsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
