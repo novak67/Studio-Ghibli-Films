4befed9168b654ad549a254b9fadb4899f34ac90
Ryan Bachrach <ryanbachrach9@gmail.com>
2020-02-05 10:50:30 -0600

    working on DescriptionViewController

--- Studio Ghibli Films/DescriptionViewController.swift
+++ Studio Ghibli Films/DescriptionViewController.swift
@@ -2,19 +2,20 @@
 //  ViewController.swift
 //  Studio Ghibli Films
 //
-//  Created by ryan bachrach on 2/3/20.
+//  Created by ryan bachrach on 2/5/20.
 //  Copyright © 2020 Novak Software Development. All rights reserved.
 //
 
 import UIKit
 
-class FilmsViewController: UITableViewController {
+class DescriptionViewController: UITableViewController {
     
-    var films = [[String: String]]()
+    var description1 = [[String: String]]()
+    var film = [String: String]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.title = "Studio Ghibli Films"
+        self.title = "Film Details"
         let query = "https://ghibliapi.herokuapp.com/films"
         DispatchQueue.global(qos: .userInitiated).async {
             [unowned self] in
@@ -30,15 +31,14 @@ class FilmsViewController: UITableViewController {
     }
     func parse(json: JSON) {
         for result in json[].arrayValue {
-            let id = result["id"].stringValue
             let title = result["title"].stringValue
             let description = result["description"].stringValue
             let director = result["director"].stringValue
             let producer = result["producer"].stringValue
             let release_date = result["release_date"].stringValue
             let rt_score = result["rt_score"].stringValue
-            let film = ["id": id, "title": title, "description": description, "director": director, "producer": producer, "release_date": release_date, "rt_score": rt_score]
-            films.append(film)
+            let film = ["title": title, "description": description, "director": director, "producer": producer, "release_date": release_date, "rt_score": rt_score]
+            description1.append(film)
         }
         DispatchQueue.main.async {
             [unowned self] in
@@ -49,20 +49,10 @@ class FilmsViewController: UITableViewController {
         DispatchQueue.main.async {
             [unowned self] in
             let alert = UIAlertController(title: "Loading Error",
-                                          message: "There was a problem loading the list",
+                                          message: "There was a problem loading the details",
                                           preferredStyle: .actionSheet)
             alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
             self.present(alert, animated: true, completion: nil)
         }
-    }
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return films.count
-    }
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        let film = films[indexPath.row]
-        cell.textLabel?.text = film["title"]
-        cell.detailTextLabel?.text = film["release_date"]
-        return cell
     }
 }
